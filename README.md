3f3de7e1a9cabd337ae3543296506b28ad22f351
abstractmachines <abstractmachines@gmail.com>
2016-08-09 13:46:41 -0700

    first. helloworld.

---
+++ README.md
@@ -0,0 +1,3 @@
+this is the readme file which contains nothing at the moment
+
+helloworld, a first express.js project. 
---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function( req, res )
+{
+	res.send('Hello World! Wesley Willis approved.');
+});
+
+app.listen( 3000, function()
+{
+	console.log('Example app is now listening on port 3000, sucka!');
+});
+
+// this app starts a server and listens on port 3000 for connections.
+// The app responds with a hello world! for requests to the root URL or ROUTE.
+// For every other path, it will respond with a 404, not found.
+
+
+// run this app with the following command: $ node app.js
