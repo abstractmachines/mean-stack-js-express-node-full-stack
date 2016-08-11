a0a6324cf5eadfcc588f094466937e47be52a1c9
abstractmachines <abstractmachines@gmail.com>
2016-08-11 14:18:38 -0700

    api methods express

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 'use strict'
 
+// this app starts a server and listens on port 3000 for connections.
+// The app responds with a hello world! for requests to the root URL or ROUTE.
+// For every other path, it will respond with a 404, not found.
+
+
+// run this app with the following commands:
+// default: $ node app.js
+// Other instances with named port(s): $ PORT=5000 node app.js
+
 var express = require('express');
 
 var app = express();
@@ -11,18 +20,13 @@ app.get('/', function( req, res )
 	res.send('Hello World! Wesley Willis approved.');
 });
 
-var port = 3000;
+var port = ( process.env.PORT || 3000 );
 
-app.listen( port, function()
-{
-	console.log('Example app is now listening on port' + port + ', sucka!');
-	console.log( process.env.port );
+// app.listen( port, function()
+// {
+// 	console.log('Example app is now listening on port' + port + ', sucka!');
+// 	console.log( process.env.PORT );
+//
+// });
 
-});
-
-// this app starts a server and listens on port 3000 for connections.
-// The app responds with a hello world! for requests to the root URL or ROUTE.
-// For every other path, it will respond with a 404, not found.
-
-
-// run this app with the following command: $ node app.js
+app.listen( port );
