2851dd1800fb29a2c597be45d4deb4b9d00bb446
abstractmachines <abstractmachines@gmail.com>
2016-09-02 16:37:47 -0700

    Verify on command line, exit code of bash script to hit server with curl.
    terminal 1: run PORT=3100 node app.js
    expected output after terminal 2 curl's:
    radsauce
    
    terminal 2:  ./verifyscript.sh && echo "it worked" || echo "fail"
    expected output:
    radsauce
    it worked

--- verifyscript.sh
+++ verifyscript.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
+#terminal 1: run PORT=3100 node app.js
+#expected output after terminal 2 curl's:
+#radsauce
+
+#terminal 2:  ./verifyscript.sh && echo "it worked" || echo "fail"
+#expected output:
+# radsauce
+# it worked
+
 OUTPUT="$(curl --request POST -d radsauce http://localhost:3100/foo -H "Content-Type: text/plain" 2>/dev/null )"
 
 echo "${OUTPUT}"
-#
-if [ "${OUTPUT}" = "radsauce" ];
 
-then
 
-  echo "gimme things 'cause it's the same: OUTPUT is ${OUTPUT}";
+if [ "${OUTPUT}" = "radsauce" ]; then
+
+  #this next line is unnecessary, just exists to clarify purpose:
+  echo "success, child process exit code 0: OUTPUT is ${OUTPUT}";
+
+  exit 0;
+
+else
+
+  #this next line is unnecessary, just exists to clarify purpose:
+  echo "failure, child process exit code 1: OUTPUT is ${OUTPUT}";
+
+  exit 1;
 
 fi
-
-#else
-#echo "nope failure";
