3c987589a714bcdad1caf2d1782399522f3caa49
abstractmachines <abstractmachines@gmail.com>
2016-09-07 11:17:42 -0700

    Improve automation: use exit codes to determine success or fail.
    1. Run in terminal 1:
    
    $PORT=3100 node app.js
    
    2. Run in terminal 2:
    
    $( ./verifyscript.sh && if [ $? -lt 0 ]; then echo "failure, exit code -1"; elif [ $? -eq 0 ]; then echo "success, exit code 0"; elif [ $? -lt 0 ]; then echo "failure, exit code less than 1"; elif [ $? -gt 1 ]; then echo "exit code greater than 1"; fi )
    
    3. Result in terminal 2: expected output:
    
    radsauce
    success, child process exit code 0: OUTPUT is radsauce
    
    4. Result in termimal 1: expected output:
    
    radsauce
    
    5. Note: the $? -lt 0 logic, meaning if process' exit code is less than 0, was used on the
    command line, in addition to logic of string comparison within verifyscript.sh.
    
    Which is most effective?

--- verifyscript.sh
+++ verifyscript.sh
@@ -4,16 +4,24 @@
 #expected output after terminal 2 curl's:
 #radsauce
 
-#terminal 2:  ./verifyscript.sh && echo "it worked" || echo "fail"
-#expected output:
+#terminal 2:  ./verifyscript.sh && echo "it worked" || echo "fail" etc
+#
+# Specific command to run in terminal 2 is:
+#
+# ( ./verifyscript.sh && if [ $? -lt 0 ];
+# then echo "failure, exit code -1";
+# elif [ $? -eq 0 ]; then echo "success, exit code 0";
+# elif [ $? -lt 0 ]; then echo "failure, exit code less than 1";
+# elif [ $? -gt 1 ]; then echo "exit code greater than 1"; fi )
 # radsauce
 # success, child process exit code 0: OUTPUT is radsauce
-# it worked
 
 OUTPUT="$(curl --request POST -d radsauce http://localhost:3100/foo -H "Content-Type: text/plain" 2>/dev/null )"
 
 echo "${OUTPUT}"
 
+# possible alternative:
+# if [ $? -eq 0 ]; then
 
 if [ "${OUTPUT}" = "radsauce" ]; then
 
